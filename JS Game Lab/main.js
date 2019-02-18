9ef44d636238bcf5a65314337f762cd435fbfce1
Meegan Schafer <45988365+schafermeegan@users.noreply.github.com>
2019-02-18 17:34:49 -0500

    Add files via upload

---
+++ JS Game Lab/main.js
@@ -0,0 +1,38 @@
+let gameAllowed;
+let answer = prompt(`Would you like to play a game?`, ``);
+
+if (answer === yes || answer === Yes) {
+    accessAllowed = true;
+} else {
+    accessAllowed = false;
+}
+alert(accessAllowed);
+
+let computerName = `Grant`;
+let userName = prompt(`What is your name?`);
+  if (userName) {
+      console.log(`Hello, ` + userName + ` you will be playing against ` + computerName + `.`);
+   } else {
+       console.log(`Hello!`);
+   }
+
+let sum = num+random
+let num = 10;
+let random = Math.floor(Math.random() * 2) - 2;
+    while (num >= 0) {
+        console.log(num);
+        num--;
+       }   
+       console.log(+ computerName + ` has ` + num + ` health left.`);
+
+let userScore = 40;
+    while(userScore > 0) {
+        console.log(userScore);
+        userScore--;
+    }
+        console.log(+ userName + ` has` + userScore + ` health left.`);
+   
+
+
+  
+  
