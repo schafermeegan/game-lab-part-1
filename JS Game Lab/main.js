953c96b953500f781c0f3c57b665d3e83e7934c1
Meegan Schafer <45988365+schafermeegan@users.noreply.github.com>
2019-02-20 01:55:26 -0500

    Add files via upload

--- JS Game Lab/main.js
+++ JS Game Lab/main.js
@@ -1,38 +1,25 @@
-let gameAllowed;
-let answer = prompt(`Would you like to play a game?`, ``);
+let doYouWantToPlay = confirm(`Would you like to play a game?`);
 
-if (answer === yes || answer === Yes) {
-    accessAllowed = true;
-} else {
-    accessAllowed = false;
+let userName = prompt(`What would you like me to call you?`);
+  console.log(`Let's get started ${userName}, you will be competing against Grant`);
+
+let playerHealthPoints = 40;
+let grantHealthPoints = 10;
+let playWins = 0;
+
+while (playerHealthPoints >= 1 && playWins < 3) {
+  playerHealthPoints -=(Math.floor(Math.random() * 2) +1);
+  grantHealthPoints -=(Math.floor(Math.random() * 2) +1);
+    console.log(`${userName} has ${playerHealthPoints} health points left.`);
+    console.log(`Grant has ${grantHealthPoints} health points left.`);
+
+  if (grantHealthPoints<=0) {
+    grantHealthPoints=10;
+    playWins+=1;
+    console.log(`${userName} you've won!`);
 }
-alert(accessAllowed);
 
-let computerName = `Grant`;
-let userName = prompt(`What is your name?`);
-  if (userName) {
-      console.log(`Hello, ` + userName + ` you will be playing against ` + computerName + `.`);
-   } else {
-       console.log(`Hello!`);
-   }
-
-let sum = num+random
-let num = 10;
-let random = Math.floor(Math.random() * 2) - 2;
-    while (num >= 0) {
-        console.log(num);
-        num--;
-       }   
-       console.log(+ computerName + ` has ` + num + ` health left.`);
-
-let userScore = 40;
-    while(userScore > 0) {
-        console.log(userScore);
-        userScore--;
-    }
-        console.log(+ userName + ` has` + userScore + ` health left.`);
-   
-
-
-  
-  
+  if (playWins===3) {
+    console.log(`Great job, you've won the whole thing!`);
+  }
+}
